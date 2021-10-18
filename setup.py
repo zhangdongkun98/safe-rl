5d25c8095106762df440d09874de5313bb203905
zhangdongkun98 <zhangdongkun98@gmail.com>
2021-10-18 10:10:15 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='safe_rl',
+    packages=find_packages(),
+    version='0.0.1',
+    author='Zhang Dongkun',
+    author_email='zhangdongkun98@gmail.com',
+    url='https://github.com/zhangdongkun98/safe-rl',
+    description='A lib for safe RL.',
+    install_requires=[
+        
+    ],
+
+    include_package_data=True
+)
